f854205b543573a3d598be28352790afa9b39003
Akshay Sinha <alt.akshay.sinha@gmail.com>
2026-01-24 13:57:42 +0530

    shadcn init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  return <h1 className="text-red-600">Hello World</h1>;
+  return <Button>Click me</Button>;
 }
