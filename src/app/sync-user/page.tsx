e00e360b6740219a9d8af0787999c2e045ce7746
Akshay Sinha <alt.akshay.sinha@gmail.com>
2026-01-25 19:12:26 +0530

    sync user

---
+++ src/app/sync-user/page.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { auth, clerkClient } from '@clerk/nextjs/server'
+import { db } from '@/server/db'
+import { notFound, redirect } from 'next/navigation'
+
+const SyncUser = async () => {
+    const { userId } = await auth()
+
+    if (!userId) {
+        throw new Error('User not found')
+    }
+
+    const client = await clerkClient()
+    const user = await client.users.getUser(userId)
+    if(!user.emailAddresses[0]?.emailAddress) {
+        return notFound()
+    }
+    await db.user.upsert({
+        where: {
+            email: user.emailAddresses[0]?.emailAddress ?? ""
+        },
+        update: {
+            imageUrl: user.imageUrl,
+            firstName: user.firstName,
+            lastName: user.lastName
+        },
+        create: {
+            id: userId,
+            email: user.emailAddresses[0]?.emailAddress ?? "",
+            imageUrl: user.imageUrl,
+            firstName: user.firstName,
+            lastName: user.lastName
+        },
+    })
+    return redirect('/dashboard')
+}
+
+export default SyncUser
