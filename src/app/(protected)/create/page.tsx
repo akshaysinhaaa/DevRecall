12e3f4501587e87bfb148cdad625b6e47919c5b0
Akshay Sinha <alt.akshay.sinha@gmail.com>
2026-01-26 22:27:42 +0530

    form creation

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Input } from '@/components/ui/input'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
@@ -10,9 +11,48 @@ type FormInput = {
 
 const CreatePage = () => {
     const {register, handleSubmit, reset} = useForm<FormInput>()
+
+    function onSubmit(data: FormInput) {
+        window.alert(data)
+        return true
+    }
+
   return (
     <div className='flex items-center gap-12 h-full justify-center'>
         <img src='/undraw_github.svg' className='h-56 w-auto'/>
+        <div>
+            <div>
+                <h1 className='font-semibold text-2xl'>
+                    Link your Github Repository
+                </h1>
+                <p className='text-sm text-muted-foreground'>
+                    Enter the URL of your repository to link it to DevRecall
+                </p>
+            </div>
+            <div className="h-4">
+                <div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Input  
+                        {...register('projectName', { required: true})}
+                        placeholder='Project name'
+                        required
+                        />
+                        <div className="h-2"></div>
+                        <Input  
+                        {...register('repoUrl', { required: true})}
+                        placeholder='Github URL'
+                        required
+                        />
+                        <div className="h-2"></div>
+                        <Input  
+                        {...register('githubToken')}
+                        placeholder='Github Token (optional)'
+                        required
+                        />
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
