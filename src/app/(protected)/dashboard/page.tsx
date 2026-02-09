fd719ecc8eba2446f9d0c71eac517cf6ad98c4f8
Akshay Sinha <alt.akshay.sinha@gmail.com>
2026-02-09 23:49:33 +0530

    dashboard rendering

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -2,12 +2,17 @@
 import React from 'react'
 import { useUser } from '@clerk/nextjs'
 import useProject from '@/hooks/use-project'
+import { Github } from 'lucide-react'
 
 const DashboardPage = () => {
     const { project } = useProject()
   return (
     <div>
-      <h1>{project?.name}</h1>
+      <div className='flex items-center justify-between flex-wrap gap-y-4'>
+        <div className='w-fit rounded-mg bg-primary px-4 py-3'>
+          <Github className='size-5 text-white' />
+        </div>
+      </div>
     </div>
   )
 }
